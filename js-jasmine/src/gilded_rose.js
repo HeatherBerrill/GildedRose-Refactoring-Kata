8b19ba58572f8c1219b2a82e15757d2af80bde1d
HeatherBerrill <heather.berrill@btinternet.com>
2021-11-11 09:06:13 +0000

    commiting from root

--- js-jasmine/src/gilded_rose.js
+++ js-jasmine/src/gilded_rose.js
@@ -1,5 +1,5 @@
 class Item {
-  constructor(name, sellIn, quality){
+  constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
@@ -7,60 +7,110 @@ class Item {
 }
 
 class Shop {
-  constructor(items=[]){
+  constructor(items = []) {
     this.items = items;
   }
-  updateQuality() {
-    for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
-    }
 
+  updateQuality() {
+    this.items.map((singleItem) => {
+      // is the quality low enough to add the points
+      const checkQualityMax = (itemQuality, addQuality) => {
+        if (itemQuality <= maxQuality - addQuality) {
+          return true;
+        } else {
+          return false;
+        }
+      };
+      // is quality high enough to remove points
+      const checkQualityMin = (itemQuality, reduceQuality) => {
+        if (itemQuality >= minQuality + reduceQuality) {
+          return true;
+        } else {
+          return false;
+        }
+      };
+
+      const maxQuality = 50;
+      const minQuality = 0;
+      const legendary = 'Sulfuras, Hand of Ragnaros';
+      const backstagePass = 'Backstage passes to a TAFKAL80ETC concert';
+      const brie = 'Aged Brie';
+      const conjured = 'Conjured Mana Cake';
+
+      //LEGENDARY PRODUCTS
+      if (singleItem.name === legendary) {
+        singleItem.quality = singleItem.quality;
+        singleItem.sellIn = singleItem.sellIn;
+
+        // BRIE PRODUCTS
+      } else if (singleItem.name === brie) {
+        if (singleItem.sellIn > 0) {
+          if (checkQualityMax(singleItem.quality, 1)) {
+            // check the quality amount is small enough to add 1
+            singleItem.quality += 1;
+          }
+        } else if (singleItem.sellIn <= 0) {
+          if (checkQualityMax(singleItem.quality, 2)) {
+            // check the quality amount is small enough to add 2
+            singleItem.quality += 2;
+          }
+        }
+        // reduce sellin days for brie
+        singleItem.sellIn -= 1;
+
+        //BACKSTAGE PASS PRODUCTS
+      } else if (singleItem.name === backstagePass) {
+        //calculate which date bracket for quality increase
+        const earlyPurchaseDate = singleItem.sellIn > 10;
+        const standardPurchaseDate =
+          singleItem.sellIn <= 10 && singleItem.sellIn > 5;
+        const latePurchaseDate =
+          singleItem.sellIn <= 5 && singleItem.sellIn > 0;
+
+        if (earlyPurchaseDate && checkQualityMax(singleItem.quality, 1)) {
+          singleItem.quality += 1;
+        } else if (
+          standardPurchaseDate &&
+          checkQualityMax(singleItem.quality, 2)
+        ) {
+          singleItem.quality += 2;
+        } else if (latePurchaseDate && checkQualityMax(singleItem.quality, 3)) {
+          singleItem.quality += 3;
+        } else {
+          singleItem.quality = minQuality;
+        }
+        // reduce sellin for backstage pass
+        singleItem.sellIn -= 1;
+
+        // CONJURED PRODUCTS
+      } else if (singleItem.name === conjured) {
+        if (checkQualityMin(singleItem.quality, 2) && singleItem.sellIn > 0) {
+          singleItem.quality -= 2;
+        } else if (
+          checkQualityMin(singleItem.quality, 4) &&
+          singleItem.sellIn <= 0
+        ) {
+          singleItem.quality -= 4;
+        }
+        // reduce sellin amount for conjured products
+        singleItem.sellIn -= 1;
+
+        // STANDARD PRODUCTS
+      } else {
+        if (checkQualityMin(singleItem.quality, 1) && singleItem.sellIn > 0) {
+          singleItem.quality -= 1;
+        } else if (
+          checkQualityMin(singleItem.quality, 2) &&
+          singleItem.sellIn <= 0
+        ) {
+          singleItem.quality -= 2;
+        }
+        // reduce sellin amount for standard products
+        singleItem.sellIn -= 1;
+      }
+    });
     return this.items;
   }
 }
-module.exports = {
-  Item,
-  Shop
-}
+
+module.exports = { Item, Shop };
